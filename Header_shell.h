e87b1080fb6e4d7f0f02831571827b008e6ff079
alexnsweet <alexnsweet@gmail.com>
2020-11-23 06:50:05 +0000

    included environ variable

--- Header_shell.h
+++ Header_shell.h
@@ -1,15 +1,21 @@
-#ifndef Header_shell_H
-#define Header_shell_H
+#ifndef HEADER_SHELL_H
+#define HEADER_SHELL_H
 
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+extern char **environ;
 
 /* String helper functions */
 int print_string(char *str);
 int _strlen(char *s);
-
+char **tokenizer(char *str, char *delim);
 
 
 
