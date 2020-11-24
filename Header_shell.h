547d86f837a8b454eb0ecc779fb1979bded44c64
alexnsweet <alexnsweet@gmail.com>
2020-11-24 06:01:58 +0000

    updated with strcat function

--- Header_shell.h
+++ Header_shell.h
@@ -16,8 +16,8 @@ extern char **environ;
 int print_string(char *str);
 int _strlen(char *s);
 char **tokenizer(char *str, char *delim);
-int _strncmp(char *s1, char *s2, int n);
+int _strncmp(char *s1, char *s2, size_t n);
 char *path_parser(char *envvar);
-
+char *_strcat(char *dest, char *src);
 
 #endif /* Header_shell_H */
