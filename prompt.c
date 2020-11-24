22086affc21efdbc53e6cfac2e7b1e4dc2882c0f
alexnsweet <alexnsweet@gmail.com>
2020-11-24 16:34:47 +0000

    updated prompt

--- prompt.c
+++ prompt.c
@@ -3,19 +3,29 @@
 int main(int ac, char **av, char **env)
 {
 	char *buffer = NULL;
-	char *path;
+	char *path, *cmd;
 	char **tokens;
 	char **paths;
 	size_t buffSize = 0;
 	int gl, i;
 	(void) ac;
+	char *teststr1, *teststr2, *teststr3;
 
 	if (isatty(STDIN_FILENO))
 	{	write (STDOUT_FILENO, "($) ", 4);	}
 	else if (isatty(STDIN_FILENO) != 1)
 	{	write (STDOUT_FILENO, "$ ", 2);	}
 
+/* test _strcat and concat */
+teststr1 = "Hello world, ";
+teststr2 = "it's nice to see you.";
 
+printf("%s\n", teststr1);
+/*
+*teststr3 = concat(teststr1, teststr2);
+
+printf("%s\n", teststr3);
+*/
 	path = path_parser("PATH=");
 	
 	paths = tokenizer(path, ":");
@@ -38,6 +48,13 @@ int main(int ac, char **av, char **env)
 			print_string(tokens[i]);
 			print_string("\n");
 		}
+/*
+	cmd = catcmd(paths, tokens);
+	
+	printf("%s\n", cmd);
+
+	execve(cmd, tokens);
+*/
 		free(paths);
 		free(tokens);	
 		free(buffer);
