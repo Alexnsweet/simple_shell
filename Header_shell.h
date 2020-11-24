c6459830e6251581893430f2393e852f2e9843f6
alexnsweet <alexnsweet@gmail.com>
2020-11-24 19:46:24 +0000

    concat proto

--- Header_shell.h
+++ Header_shell.h
@@ -21,4 +21,5 @@ char *path_parser(char *envvar);
 char *_strcat(char *dest, char *src);
 char *catcmd(char **paths, char **tokens);
 char *_strcpy(char *dest, char *src);
+char *concat(char *str, char *str2);
 #endif /* Header_shell_H */
