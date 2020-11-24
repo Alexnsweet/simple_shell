4d7ade31d59bd4038c1f70b37ab197a96654c9ac
alexnsweet <alexnsweet@gmail.com>
2020-11-24 04:15:42 +0000

    parses the path and returns char pointer after n bytes if strings match

---
+++ path_parser.c
@@ -0,0 +1,28 @@
+#include "Header_shell.h"
+
+/**
+ * path_parser - searches for n byes of env variable and returns values
+ * @envvar: name of environment variable to search for
+ *
+ * Return: string of values for environment variable
+ */
+char *path_parser(char *envvar)
+{
+	int i, j, k;
+	char *ptr = NULL, *ptr2 NULL;
+
+	k = _strlen(*envvar);
+
+	for (i = 0, j = 0, k = 0; environ[i]; i++)
+	{
+		j = _strncmp(environ[i], *envvar, k);
+
+		if (j == 0)
+		{	ptr = &environ[i][k]; /* return string after =  */
+			return (ptr);
+		}
+	}
+	print_string("could not find variable");
+	return (ptr);
+}	
+	
