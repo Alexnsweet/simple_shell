fb5e9dc90cb39b85adea46059c0321772ea279d1
alexnsweet <alexnsweet@gmail.com>
2020-11-25 08:44:17 +0000

    fixed spaces and formatting

--- prompt.c
+++ prompt.c
@@ -2,33 +2,20 @@
 
 int main(void)
 {
-	char *buffer = NULL;
-	char *path = NULL;
-	char *cmd = NULL;
-	char **tokens;
-	char **paths;
+	char *buffer = NULL, *path = NULL, *cmd = NULL;
+	char **tokens, **paths;
 	size_t buffSize = 0;
 	int i, gl, status;
-
 	pid_t child;
 
 	_ttyprompt(); /* 0 if interactive or 1 if non-interactive */
 	path = path_parser("PATH="); /* string of path keys with delimeters */
 	paths = tokenizer(path, ":"); /* returns array of char pointers */
-
-
 	gl = getline(&buffer, &buffSize, stdin);
-/*	if (gl == -1)
-	{	print_string("could not read line");
-	}
-*/	while (gl > 0)
+	while (gl > 0)
 	{
 		tokens = tokenizer(buffer, " \n\r\f\v");
-
-		/*begin child process */
-
 		child = fork();
-
 		if (child == 0)
 		{
 			for (i = 0; paths[i]; i++)
@@ -36,15 +23,10 @@ int main(void)
 				if (execve(tokens[0], tokens, environ) == -1)
 				{
 				cmd = concat(paths[i], tokens[0]);
-/*
-				printf("cmd: %s\n", cmd);
-				printf("cmd: %s\n", cmd);
-*/
 				execve(cmd, tokens, environ);
 				}
 				free(cmd);
 			}
-
 			print_string("Command not found, buddy. Try again\n");
 			free(paths);
 			free(tokens);
@@ -53,14 +35,10 @@ int main(void)
 		}
 		else
 		{	wait(&status);	}
-
-
 			free(tokens);	
-
 		_ttyprompt();
 		gl = getline(&buffer, &buffSize, stdin);
 	}
-
 		free(buffer);
 		free(paths);
 	return (0);
