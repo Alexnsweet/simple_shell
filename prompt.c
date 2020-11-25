d4c5b4c7d4e5583c5073049ec305301a1ec8866b
alexnsweet <alexnsweet@gmail.com>
2020-11-25 02:24:38 +0000

    changed main prototype

--- prompt.c
+++ prompt.c
@@ -1,6 +1,6 @@
 #include "Header_shell.h"
 
-int main(int ac, char **av, char **env)
+int main(void)
 {
 	char *buffer = NULL;
 	char *path = NULL;
@@ -9,57 +9,56 @@ int main(int ac, char **av, char **env)
 	char **paths;
 	size_t buffSize = 0;
 	int gl, i, status, tty;
-	(void) ac;
+
 	char *teststr1, *teststr2, *teststr3;
 	pid_t child;
 
-	tty = _ttyprompt();
-	path = path_parser("PATH=");	
-	paths = tokenizer(path, ":");
+	tty = _ttyprompt(); /* 0 if interactive or 1 if non-interactive */
+	path = path_parser("PATH="); /* string of path keys with delimeters */
+	paths = tokenizer(path, ":"); /* returns array of char pointers */
 
 	gl = getline(&buffer, &buffSize, stdin);
-	if (gl == -1)
+/*	if (gl == -1)
 	{	print_string("could not read line");
 	}
-	else
+*/	if (gl > 0)
 	{
 		tokens = tokenizer(buffer, " \n\r\f\v");
 
-/*begin child process */
+		/*begin child process */
 
-child = fork();
+		child = fork();
 
-if (child == 0)
-{
-	for (i = 0; paths[i]; i++)
-	{
+		if (child == 0)
+		{
+			for (i = 0; paths[i]; i++)
+			{
 
-		cmd = concat(paths[i], tokens[0]);
+				cmd = concat(paths[i], tokens[0]);
 /*
-		printf("cmd: %s\n", cmd);
-		printf("cmd: %s\n", cmd);
+				printf("cmd: %s\n", cmd);
+				printf("cmd: %s\n", cmd);
 */
-		execve(cmd, tokens, environ);
+				execve(cmd, tokens, environ);
 
-		free(cmd);
+				free(cmd);
+			}
+
+			print_string("Command not found, buddy. Try again\n");
+			free(paths);
+			free(tokens);
+			free(buffer);
+
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{	wait(&status);	}
+
+			free(paths);
+			free(tokens);	
+			free(buffer);
+
 	}
-
-		print_string("Command not found, buddy. Try again\n");
-		free(paths);
-		free(tokens);
-		free(buffer);
-		exit(EXIT_SUCCESS);
-}
-else
-{
-	wait(&status);
-}
-
-		free(paths);
-		free(tokens);	
-		free(buffer);
-
-}
 
 	return (0);
 }
